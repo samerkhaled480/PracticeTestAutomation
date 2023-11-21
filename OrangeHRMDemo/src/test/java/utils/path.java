96a28f4cecbc01281a31e65463f8f362b14229f0
SamerKhaled <SamerKhaled480@gmail.com>
2023-11-21 23:50:18 +0200

    deletefiles

--- OrangeHRMDemo/src/test/java/utils/path.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class path {
-	
-	public static final String SWAGLABS_LOGINCSV  = "D:\\samer\\Practice_Selenium\\DemoProject\\src\\test\\resources\\csv\\orangehrmdemo.csv";
-	public static final String SWAGLABS_LOGINJSON  ="D:\\samer\\Practice_Selenium\\DemoProject\\src\\test\\resources\\json\\orangehrmdemo.json";
-
-
-}
